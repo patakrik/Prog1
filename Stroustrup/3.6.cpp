3ab11280f2e36228f26e012e888ea8795f34c5ca
Patrik <36334711+patakrik@users.noreply.github.com>
2018-05-08 22:30:42 +0200

    Create 3.6.cpp

---
+++ Stroustrup/3.6.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	bool a;
+	char b;
+	int c;
+	double d;
+	string e;
+
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	cin >> d;
+	cin >> e;
+	
+	return 0;
+}
