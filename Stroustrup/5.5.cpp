0022f74eb5592b2f7c09f741a603c47d783fb3e9
Patrik <36334711+patakrik@users.noreply.github.com>
2018-05-08 22:32:08 +0200

    Create 5.5.cpp

---
+++ Stroustrup/5.5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string name;
+	int age;
+
+	cin >> name;
+	cin >> age;
+
+	cout << "Name is: " << name << "\t" << "Age is: " << age << endl;
+
+	return 0;
+}
