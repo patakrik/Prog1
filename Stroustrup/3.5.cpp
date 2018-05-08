e8bbcf8a3ff20bf39cdcad2d7b7db54d40319e7f
Patrik <36334711+patakrik@users.noreply.github.com>
2018-05-08 22:22:56 +0200

    Create 3.5.cpp

---
+++ Stroustrup/3.5.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	bool a = true;
+	char b = 'b';
+	int c = 3;
+	double d = 3.14;
+	string e = "Programozzunk.";
+
+	cout << a << endl;
+	cout << b << endl;
+	cout << c << endl;
+	cout << d << endl;
+	cout << e << endl;
+	
+	return 0;
+}
